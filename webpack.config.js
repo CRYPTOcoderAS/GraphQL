9e7c78f850a2126f55b3ccf1ef7e6b5ec5d8d13d
Akshat Sachan <156008837+akshat-sachan1@users.noreply.github.com>
2025-02-24 02:15:20 +0530

    :zap: FIX

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+  target: 'node',
+  mode: 'production',
   optimization: { minimize: false },
+  node: {
+    __dirname: false,
+    __filename: false
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!(mongoose|@apollo)\/)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [['@babel/preset-env', { targets: { node: '18' } }]],
+            presets: [
+              ['@babel/preset-env', {
+                targets: { node: '20' },
+                modules: 'commonjs'
+              }]
+            ],
+            plugins: [
+              '@babel/plugin-proposal-nullish-coalescing-operator',
+              '@babel/plugin-proposal-optional-chaining'
+            ]
           },
         },
       },
@@ -18,6 +34,12 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js'],
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
+  externals: ['aws-sdk', 'mongodb-client-encryption'],
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^(?:async_hooks|fs|path|url|stream|crypto|http|net|dns|os|tls|https|zlib|events|util|buffer)$/
+    })
+  ]
 };
